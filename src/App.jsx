837dcd0612e965cdd6edfaa22aef089c82d44e94
nandala-allan <andala63@gmail.com>
2023-07-31 10:16:06 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div className='search'>
+    {/* <FontAwesomeIcon icon="fa-brands fa-github" /> */}
+  
+      <form>
+      <label htmlFor="username">Find Github User Profile</label>
+        <input type="text" name="username"/>
+      </form>
+    </div>
+  )
+}
+
+export default App
