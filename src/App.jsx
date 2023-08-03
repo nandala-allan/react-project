8b7da9f4d3e54ef474a550c40e45133aebf7f546
nandala-allan <andala63@gmail.com>
2023-08-03 13:54:41 +0300

    edit mobile views, add fav icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,38 @@
-import { useNavigate} from 'react-router-dom';
-import { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub } from "@fortawesome/free-brands-svg-icons"
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
-
-import './App.css'
+import "./App.css";
 
 function App() {
-
-  let [username, setUsername] = useState('');
-  let handleChange = e => setUsername(e.target.value);
+  let [username, setUsername] = useState("");
+  let handleChange = (e) => setUsername(e.target.value);
   let redirectPage = useNavigate();
 
-
-
   function searchUser() {
-    localStorage.setItem('git-username', username);
-    redirectPage('/profile')
+    localStorage.setItem("git-username", username);
+    redirectPage("/profile");
   }
 
-  
-
   return (
-
     <div>
-      
-      <div className='search'>
-     <FontAwesomeIcon icon={faGithub} className='octo-cat'/>
-      <div></div>
+      <div className="search">
+        <FontAwesomeIcon icon={faGithub} className="octo-cat" />
+        <div></div>
         <form onSubmit={searchUser}>
-      <label htmlFor="username"> Search Github user profile</label>
-      <input type="text" name="username"  onChange={handleChange}
-          value={username} required/>
-      </form>
+          <label htmlFor="username"> Search Github user profile</label>
+          <input
+            type="text"
+            name="username"
+            onChange={handleChange}
+            value={username}
+            required
+          />
+        </form>
+      </div>
     </div>
-    </div>
-    
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
-import App from './App.jsx'
-import Profile from './components/Profile.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
+import Profile from "./components/Profile.jsx";
+import "./index.css";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <App />
+    path: "/",
+    element: <App />,
   },
   {
-    path: '/profile',
-    element: <Profile />
-  }
+    path: "/profile",
+    element: <Profile />,
+  },
+]);
 
-])
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
